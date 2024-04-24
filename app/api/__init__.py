4638cce2cd05ee0e265612fba5e8c0c3c89b1554
dheeraj hegde <dheeraj.r.hegde@gmail.com>
2024-04-24 11:24:10 -0400

    Intiial commit

---
+++ app/api/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+# Additional API routes and functionality can be defined here
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+# This file is intentionally left blank.
+# You can use this file to define data models or database schemas for your FastAPI application.
+# For example, you can define a User model or a Product model here.
+# You can import and use these models in other parts of your application, such as in your API routes or services.
+# Feel free to add your own code here based on your project requirements.
